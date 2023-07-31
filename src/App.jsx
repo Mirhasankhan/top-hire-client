b4c8c62e32ab870308ceb8f9245abc8c42513f29
Mirhasankhan <mirhasan000034@gmail.com>
2023-08-01 02:43:56 +0600

    project setup done

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Button } from 'react-bootstrap';
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    <div>
+    <Button>How ARE YOU</Button>
+    </div>
+    </>
+  )
+}
+
+export default App
